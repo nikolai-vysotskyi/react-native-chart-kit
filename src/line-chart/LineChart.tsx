15047ae3b0a94e2b9c3b633aee6afe5bcb8c0f30
Nikolai <nikolai.vysotskyi@gmail.com>
2022-07-06 10:53:07 +0300

    remove last item space

--- src/line-chart/LineChart.tsx
+++ src/line-chart/LineChart.tsx
@@ -650,7 +650,7 @@ class LineChart extends AbstractChart<LineChartProps, LineChartState> {
 
   getXMaxValues = (data: Dataset[]) => {
     return data.reduce((acc, cur) => {
-      return cur.data.length > acc ? cur.data.length : acc;
+      return cur.data.length > acc ? cur.data.length - 1: acc;
     }, 0);
   };
 
